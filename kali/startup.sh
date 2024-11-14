524a7c069e3a39ecef8ed8c65b0b4622af6b425b
TimK011 <timkoll96@gmail.com>
2024-11-14 16:49:46 +0100

    First Structure on Ubuntu

--- kali/startup.sh
+++ kali/startup.sh
@@ -7,6 +7,9 @@ mkdir -p /root/.vnc
 echo "kaliadmin" | vncpasswd -f > /root/.vnc/passwd
 chmod 600 /root/.vnc/passwd
 
+# Netzwerkschnittstelle in den Promiscuous-Modus setzen
+ip link set eth0 promisc on
+
 vncserver :1 -geometry 1280x800 -depth 24
 
 tail -f /dev/null
