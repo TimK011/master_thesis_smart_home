fb78f45db45bf65d1ae49c60490dd57960937501
TimK011 <timkoll96@gmail.com>
2024-11-20 20:02:55 +0100

    Including necessary passwordfile and updates for kali

--- kali/startup.sh
+++ kali/startup.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 export USER=root
 
+# Starten des SSH-Dienstes
 service ssh start
 
+# VNC-Konfiguration
 mkdir -p /root/.vnc
 echo "kaliadmin" | vncpasswd -f > /root/.vnc/passwd
 chmod 600 /root/.vnc/passwd
 
-# Set the eth0 network interface to promiscuous mode
+# Setzen des eth0 Netzwerkinterfaces in den Promiscuous Mode
 ip link set eth0 promisc on
 
-# Start the VNC server on display :1 with a resolution of 1280x800 and a color depth of 24
-vncserver :1 -geometry 1280x800 -depth 24
+# Starten des VNC-Servers auf Display :1 mit einer Auflösung von 1280x800 und Farbtiefe 24
+vncserver :1 -geometry 1280x800 -depth 24 &
+
+# Optional: Starten von Burp Suite automatisch
+ java -jar /opt/burp/burpsuite_community.jar &
 
 # Keep the script running indefinitely
 tail -f /dev/null
